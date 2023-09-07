ed5c5e6ed4266045255c4b25b8fecdba3afb7538
Jack Ogozaly <72467438+JackOgozaly@users.noreply.github.com>
2023-09-07 10:00:14 -0400

    Update streamlit_agcensus.py

--- streamlit_agcensus.py
+++ streamlit_agcensus.py
@@ -348,8 +348,12 @@ if prompt := st.chat_input("What is your question?"):
                     #Quirks
                     if 'Value' in api_data.columns:
                         fake_typing(f"Data successfully pulled from NASS API with {api_data.shape[0]} rows and {api_data.shape[1]} columns")
-                        fake_typing(f"{format(len(api_data[api_data['Value'].str.strip() == '(D)']) / len(api_data), '.0%')} of rows in the pulled data contain redacted information, this may skew the analysis")
-                      
+                        percent_null = len(api_data[api_data['Value'].str.strip() == '(D)']) / len(api_data)
+                        if percent_null < .2:
+                            fake_typing(f"{format(percent_null, '.0%')}% of rows in the pulled data contain redacted information, this may slightly skew the analysis")
+                        else:
+                            fake_typing(f"{format(percent_null, '.0%')}% of rows in the pulled data contain redacted information, this may heavily skew the analysis")
+                        
                         api_data['Value'] = api_data['Value'].str.replace(',', '', regex=False)
                         api_data['Value'] = api_data['Value'].str.replace('(NA)', '', regex=False)
                         api_data['Value'] = api_data['Value'].str.replace('()', '', regex=False)
